09f2649367dc7f979f0b80e856c45d93b93989fc
Nat Peterson <natpeterson@gmail.com>
2022-12-02 18:57:43 -0800

    day2 tidy

--- day2.mjs
+++ day2.mjs
@@ -4,18 +4,21 @@ const sample = `A Y
 B X
 C Z`;
 
-// score = choice + outcome
-// 1 rock, 2 paper, 3 scissors
-// 0 lost, 3 draw, 6 won
+// score:  choice + outcome
+// choices: 1 rock, 2 paper, 3 scissors
+// outcomes: 0 lose, 3 draw, 6 win
 
-const POINTS = { X: 1, Y: 2, Z: 3, A: 1, B: 2, C: 3 };
-
-const GAMES = { AY: 6, BZ: 6, CX: 6, AX: 3, BY: 3, CZ: 3, AZ: 0, BX: 0, CY: 0 };
-
-// part2 X = 0, Y = 3, Z = 6
-//const PART2 = {X: 0, Y: 3, Z: 6}
-
-const PART2 = { AY: 4, BZ: 9, CX: 2, AX: 3, BY: 5, CZ: 7, AZ: 8, BX: 1, CY: 6 };
+const GAMES = {
+  AY: 6 + 2, // paper beats rock
+  BZ: 6 + 3, // scissors beats paper
+  CX: 6 + 1, // rock beats scissors
+  AX: 3 + 1, // rock
+  BY: 3 + 2, // paper
+  CZ: 3 + 3, // scissors
+  AZ: 0 + 3, // scissors loses to rock
+  BX: 0 + 1, // rock loses to paper
+  CY: 0 + 2, // paper loses to scissors
+};
 
 let puzzle = sample;
 const dataBuf = await promises.readFile("input2");
@@ -23,30 +26,24 @@ puzzle = dataBuf.toString();
 const games = puzzle
   .split("\n")
   .filter((line) => Boolean(line))
-  .map((game) => game.split(" "));
-//console.log(games);
+  .map((game) => game.split(" ").join(""));
 
-const part1 = games
-  .map((game) => GAMES[game.join("")] + POINTS[game[1]])
-  .reduce((a, b) => a + b);
+const part1 = games.map((game) => GAMES[game]).reduce((a, b) => a + b);
 
-console.log("#1", part1); // wrong 12865
+console.log("#1", part1);
 
-const part2 = games.map((game) => PART2[game.join("")]).reduce((a, b) => a + b);
+const PART2 = {
+  AY: 1 + 3, // draw + rock
+  BY: 2 + 3, // draw + paper
+  CY: 3 + 3, // draw + scissors
+  AX: 3 + 0, // lose + scissors (loses to rock)
+  BX: 1 + 0, // lose + rock (loses to paper)
+  CX: 2 + 0, // lose + paper (loses to scissors)
+  AZ: 2 + 6, // win + paper (beats rock)
+  BZ: 3 + 6, // win + scissors (beats paper)
+  CZ: 1 + 6, // win + rock (beats scissors)
+};
 
-console.log("#2", part2); // wrong 12944
+const part2 = games.map((game) => PART2[game]).reduce((a, b) => a + b);
 
-/*
-const calories = elves
-  .map((elf) =>
-    elf
-      .split("\n")
-      .map((n) => Number(n))
-      .reduce((a, b) => a + b)
-  )
-  .sort((a, b) => b - a);
-console.log("#1", calories[0]);
-
-const part2 = calories.slice(0, 3).reduce((a, b) => a + b);
 console.log("#2", part2);
-*/
