7d42f437a86be689a71c3aa1e10ea573681c1198
Nat Peterson <natpeterson@gmail.com>
2022-12-05 08:13:51 -0800

    tidy day5

--- day5.mjs
+++ day5.mjs
@@ -15,78 +15,52 @@ const dataBuf = await promises.readFile("input5");
 puzzle = dataBuf.toString();
 const parts = puzzle.split("\n\n");
 
+// extract diagram of initial state and move instructions
 const diag = parts[0].split("\n").filter((line) => Boolean(line));
 const inst = parts[1].split("\n").filter((line) => Boolean(line));
-diag.reverse();
+diag.reverse(); // to build arrays in order
 
-function solve1() {
-  let stackIds = [...diag[0].matchAll(/\d+/g)];
-  const pos = new Map();
-  const stacks = new Array();
-  stackIds.forEach((m) => {
-    pos.set(Number(m[0]), m.index);
-    stacks.push(new Array());
+// identify stack ids and char position
+let stackIds = [...diag[0].matchAll(/\d+/g)];
+// build pos map {id->index} and stacks array
+const pos = new Map();
+let stacks = new Array();
+stackIds.forEach((m) => {
+  pos.set(Number(m[0]), m.index);
+  stacks.push(new Array());
+});
+
+// populate stacks
+diag.slice(1).forEach((line) => {
+  stacks.forEach((arr, i) => {
+    let idx = pos.get(i + 1);
+    if (line.charAt(idx) != " ") arr.push(line.charAt(idx));
   });
-  //console.log(pos);
+});
 
-  diag.slice(1).forEach((line) => {
-    stacks.forEach((arr, i) => {
-      let idx = pos.get(i + 1);
-      if (line.charAt(idx) != " ") arr.push(line.charAt(idx));
-    });
-  });
+// parse moves into [num, from, to]
+const moves = inst.map((inst) =>
+  [...inst.matchAll(/\d+/g)].map((m) => Number(m[0]))
+);
+// deep copy stacks so we can reset for part2
+const stacksPart1 = stacks.map((stack) => stack.slice());
 
-  //console.log(stacks);
+// part 1
+moves.forEach(([num, from, to]) => {
+  // move between stacks 1 at a time
+  for (let i = 0; i < num; i++) {
+    stacksPart1[to - 1].push(stacksPart1[from - 1].pop());
+  }
+});
 
-  const moves = inst.map((inst) =>
-    [...inst.matchAll(/\d+/g)].map((m) => Number(m[0]))
-  );
-  //console.log(moves);
-  moves.forEach(([num, from, to]) => {
-    for (let i = 0; i < num; i++) {
-      stacks[to - 1].push(stacks[from - 1].pop());
-    }
-  });
-  //console.log(stacks);
+let part1 = stacksPart1.map((arr) => arr.slice(-1).pop()).join("");
+console.log("#1", part1);
 
-  let part1 = stacks.map((arr) => arr.slice(-1).pop()).join("");
-  return part1;
-}
+// part 2
+moves.forEach(([num, from, to]) => {
+  // move num stacks together from->to
+  stacks[to - 1].push(...stacks[from - 1].splice(-num));
+});
 
-function solve2() {
-  let stackIds = [...diag[0].matchAll(/\d+/g)];
-  const pos = new Map();
-  const stacks = new Array();
-  stackIds.forEach((m) => {
-    pos.set(Number(m[0]), m.index);
-    stacks.push(new Array());
-  });
-  //console.log(pos);
-
-  diag.slice(1).forEach((line) => {
-    stacks.forEach((arr, i) => {
-      let idx = pos.get(i + 1);
-      if (line.charAt(idx) != " ") arr.push(line.charAt(idx));
-    });
-  });
-
-  //console.log(stacks);
-
-  const moves = inst.map((inst) =>
-    [...inst.matchAll(/\d+/g)].map((m) => Number(m[0]))
-  );
-  //console.log(moves);
-  moves.forEach(([num, from, to]) => {
-    //console.log(`move ${num} from ${from} to ${to}: ${stacks[from - 1]}`);
-    const moved = stacks[from - 1].splice(-num);
-    //console.log("moved", moved);
-    moved.forEach((box) => stacks[to - 1].push(box));
-    //console.log(stacks);
-  });
-
-  let part2 = stacks.map((arr) => arr.slice(-1).pop()).join("");
-  return part2;
-}
-
-console.log("#1", solve1());
-console.log("#2", solve2());
+let part2 = stacks.map((arr) => arr.slice(-1).pop()).join("");
+console.log("#2", part2);
