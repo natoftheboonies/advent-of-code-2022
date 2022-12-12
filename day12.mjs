7065ce8beb4a09d13c29cba85ded86703fddf820
Nat Peterson <natpeterson@gmail.com>
2022-12-12 07:54:48 -0800

    day12 tidy

--- day12.mjs
+++ day12.mjs
@@ -22,12 +22,12 @@ const topo = lines.map((line) =>
   line.split("").map((tree) => Number(tree.charCodeAt() - 97))
 );
 
-//console.log(topo);
+// inspect maze
+const dim = { row: topo.length, col: topo[0].length };
 
 let start, goal;
 topo.forEach((row, y) =>
   row.forEach((c, x) => {
-    //console.log(c, "E".charCodeAt() - 97);
     if (c == "S".charCodeAt() - 97) {
       start = [x, y];
       topo[y][x] = 0;
@@ -39,39 +39,27 @@ topo.forEach((row, y) =>
   })
 );
 
-console.log(start, goal);
-
-const dim = { row: topo.length, col: topo[0].length };
-console.log("maze dim", dim);
-
+// need integer keys, so use y*(dim.col)+x
 const toKey = ([x, y]) => y * dim.col + x;
 const fromKey = (key) => [key % dim.col, Math.floor(key / dim.col)];
 
-// copied from 2021 day 15
-function djikstra() {
-  //const goal = [dim.col * mult - 1, dim.row * mult - 1];
+// inspired by 2021 day 15
+function djikstra(
+  start = [0, 0],
+  goalTest = ([x, y]) => topo[y][x] == 26,
+  validMove = ([x, y], [hx, hy]) => topo[y][x] - topo[hy][hx] <= 1
+) {
   const shortest = new Map([start]);
-  console.log("goal is", goal);
-  const mult = 1;
-
   const heap = new MinQueue(1024);
-  // need integer keys, so use y*(dim.col*mult)+x
   heap.push(toKey(start), 0);
 
-  let maxDepth = 0;
-
   while (heap.size) {
     const base_cost = heap.peekPriority();
     const last = heap.pop();
     const [hx, hy] = fromKey(last);
-    // const hy = Math.floor(last / (dim.row * mult));
-    // const hx = last % (dim.row * mult);
-    if (hx == goal[0] && hy == goal[1]) {
-      console.log("goal!", heap.size);
-
+    if (goalTest([hx, hy])) {
       return base_cost;
     }
-    // for (const [dx, dy] of DIRS) {
     DIRS.forEach(([dx, dy]) => {
       const x = hx + dx;
       const y = hy + dy;
@@ -79,72 +67,26 @@ function djikstra() {
       if (x >= dim.col || x < 0 || y >= dim.row || y < 0) return;
       if (hx >= dim.col || hx < 0 || hy >= dim.row || hy < 0) return;
       // illegal climb
-      if (topo[y][x] - topo[hy][hx] > 1) return;
-      //console.log("valid", y, x, "from", hy, hx);
+      if (!validMove([x, y], [hx, hy])) return;
       const key = toKey([x, y]);
-      if (topo[y][x] > maxDepth) maxDepth = topo[y][x];
 
-      let cost = 1;
       // new cost for this node
-      const neighbor_cost = base_cost + cost;
+      const neighbor_cost = base_cost + 1;
       if ((shortest.get(key) ?? 9999) > neighbor_cost) {
-        //console.log("set", key, "priority", neighbor_cost);
         shortest.set(key, neighbor_cost);
         heap.push(key, neighbor_cost);
       }
     });
   }
-  console.log("game over", maxDepth);
+  console.error("goal not found");
 }
 
-console.log("part1", djikstra());
+console.log("#1:", djikstra());
 
-// search from goal to height 0
-function part2() {
-  //const goal = [dim.col * mult - 1, dim.row * mult - 1];
-  const shortest = new Map([goal]);
-
-  const heap = new MinQueue(1024);
-  // need integer keys, so use y*(dim.col*mult)+x
-  heap.push(toKey(goal), 0);
-
-  let maxDepth = 0;
-
-  while (heap.size) {
-    const base_cost = heap.peekPriority();
-    const last = heap.pop();
-    const [hx, hy] = fromKey(last);
-    // const hy = Math.floor(last / (dim.row * mult));
-    // const hx = last % (dim.row * mult);
-    if (topo[hy][hx] == 0) {
-      console.log("goal!", heap.size, hx, hy);
-
-      return base_cost;
-    }
-    // for (const [dx, dy] of DIRS) {
-    DIRS.forEach(([dx, dy]) => {
-      const x = hx + dx;
-      const y = hy + dy;
-      // out of bounds
-      if (x >= dim.col || x < 0 || y >= dim.row || y < 0) return;
-      if (hx >= dim.col || hx < 0 || hy >= dim.row || hy < 0) return;
-      // illegal climb
-      if (topo[y][x] - topo[hy][hx] < -1) return;
-      //console.log("valid", y, x, "from", hy, hx);
-      const key = toKey([x, y]);
-      if (topo[y][x] > maxDepth) maxDepth = topo[y][x];
-
-      let cost = 1;
-      // new cost for this node
-      const neighbor_cost = base_cost + cost;
-      if ((shortest.get(key) ?? 9999) > neighbor_cost) {
-        //console.log("set", key, "priority", neighbor_cost);
-        shortest.set(key, neighbor_cost);
-        heap.push(key, neighbor_cost);
-      }
-    });
-  }
-  console.log("game over", maxDepth);
-}
-
-console.log("#2:", part2());
+// search from peak to low points
+const part2 = djikstra(
+  goal,
+  ([x, y]) => topo[y][x] == 0,
+  ([x, y], [hx, hy]) => topo[y][x] - topo[hy][hx] >= -1
+);
+console.log("#2:", part2);
