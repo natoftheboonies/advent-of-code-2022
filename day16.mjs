bbc0179e0f00d20a1f7dc37c62d6646e36c06bbf
Nat Peterson <natpeterson@gmail.com>
2022-12-16 16:16:23 -0800

    day16 part2

--- day16.mjs
+++ day16.mjs
@@ -22,101 +22,135 @@ if (true) {
 
 const lines = puzzle.split("\n").filter((line) => Boolean(line));
 
-const nodes = new Object();
-const edges = new Object();
+// parse into map of room -> {room, rate, edges}
+const rooms = new Map(
+  lines.map((line) => {
+    let valve = line.substring(6, 8);
+    let rate = Number(line.substring(line.indexOf("=") + 1, line.indexOf(";")));
+    let tunnels = line
+      .substring(line.indexOf(" ", line.indexOf("valve")) + 1)
+      .split(",")
+      .map((t) => t.trim());
+    return [
+      valve,
+      {
+        valve,
+        rate,
+        edges: Object.fromEntries(tunnels.map((tunnel) => [tunnel, 1])),
+      },
+    ];
+  })
+);
 
-const rooms = lines.map((line) => {
-  let valve = line.substring(6, 8);
-  let rate = Number(line.substring(line.indexOf("=") + 1, line.indexOf(";")));
-  let tunnels = line
-    .substring(line.indexOf(" ", line.indexOf("valve")) + 1)
-    .split(",")
-    .map((t) => t.trim());
-  nodes[valve] = rate;
-  edges[valve] = Object.fromEntries(tunnels.map((tunnel) => [tunnel, 1]));
-  return [valve, rate, tunnels];
-  //console.log(valve, rate, tunnels);
-});
+const start = "AA";
 
-console.assert(rooms.length == lines.length);
+console.assert(rooms.size == lines.length);
 
 // porting https://www.reddit.com/r/adventofcode/comments/zn6k1l/comment/j0gqhgs/?context=3
-// build a map of useful rooms to a map of distance to other rooms
-// {AA: {DD: 1, BB: 1, CC: 2, EE: 2, JJ: 2, HH: 5}
+// build a map of useful rooms to an object of distance to other rooms
+// AA => {DD: 1, BB: 1, CC: 2, EE: 2, JJ: 2, HH: 5}
 
-function findBestPaths(edges, goal) {
+function findBestPaths(goal) {
   const q = [[0, goal]];
-  const path_lengths = Object.fromEntries([[goal, 0]]);
+  const bestPaths = Object.fromEntries([[goal, 0]]);
   while (q.length > 0) {
-    const [cost, current] = q.pop();
-    for (const [point, point_cost] of Object.entries(edges[current])) {
-      if (!(point in path_lengths) || cost + point_cost < path_lengths[point]) {
-        path_lengths[point] = cost + point_cost;
-        q.push([cost + point_cost, point]);
+    const [priorCost, current] = q.pop();
+    for (const [point, cost] of Object.entries(rooms.get(current).edges)) {
+      const newCost = priorCost + cost;
+      if (!(point in bestPaths) || newCost < bestPaths[point]) {
+        bestPaths[point] = newCost;
+        q.push([newCost, point]);
       }
     }
   }
-  for (const point in path_lengths) {
-    if (nodes[point] == 0) delete path_lengths[point];
+  // remove records for useless rooms
+  for (const point in bestPaths) {
+    if (rooms.get(point).rate == 0) delete bestPaths[point];
   }
-  return path_lengths;
+  return bestPaths;
 }
 
-function* all_orders(distances, node, todo, done, time) {
-  for (const next_node of todo) {
-    //console.log("todo", todo, done);
-    const cost = distances[node][next_node] + 1;
+// for start room and other rooms with a useful valve,
+// update distance (time) to all other useful rooms.
+for (const room of rooms.values()) {
+  if (room.valve == start || room.rate > 0) {
+    room.edges = findBestPaths(room.valve);
+  }
+}
+
+// starting at `room` generate all paths through `visit` rooms valid for remaining time
+function* generatePaths(room, visit, done, time) {
+  for (const next of visit) {
+    const cost = rooms.get(room).edges[next] + 1;
     if (cost < time) {
-      for (let value of all_orders(
-        distances,
-        next_node,
-        new Set([...todo].filter((x) => x != next_node)),
-        done.concat([next_node]),
+      for (let value of generatePaths(
+        next,
+        visit.filter((v) => v != next),
+        done.concat([next]),
         time - cost
-      ))
+      )) {
         yield value;
+      }
     }
   }
   yield done;
 }
 
-function run_order(costs, start_node, order, t) {
-  let release = 0;
-  let current = start_node;
-  for (const node of order) {
-    if (!(current in costs) || !(node in costs[current])) {
-      console.log("no", nodes);
-      console.error("urk", current, node);
-    }
-    const cost = costs[current][node] + 1;
-    t -= cost;
-    console.assert(t > 0);
-    release += t * nodes[node];
-    current = node;
+// for a given path, compute the pressure released
+function pressureForPath(path, time) {
+  let pressure = 0;
+  let room = start;
+  for (const step of path) {
+    // move to room and open valve (+1)
+    const cost = rooms.get(room).edges[step] + 1;
+    time -= cost;
+    console.assert(time > 0);
+    // add remaining time for this room
+    pressure += time * rooms.get(step).rate;
+    room = step;
   }
-  return release;
+  return pressure;
 }
 
-const foo = Object.fromEntries(
-  Object.keys(nodes)
-    .filter((valve) => valve == "AA" || nodes[valve] > 0)
-    .map((valve) => [valve, findBestPaths(edges, valve)])
-);
-//console.log(foo);
+// only visiting rooms with useful valves
+const visit = [...rooms.values()]
+  .filter((room) => room.rate > 0)
+  .map((room) => room.valve);
 
-const working_nodes = new Set();
-for (const node in nodes) {
-  if (nodes[node] > 0) working_nodes.add(node);
+// part1
+
+let time = 30;
+let maxPressure = 0;
+for (const path of generatePaths(start, visit, [], time)) {
+  const pressure = pressureForPath(path, time);
+  if (pressure > maxPressure) maxPressure = pressure;
 }
 
-const start_node = "AA";
-const p1_orders = all_orders(foo, start_node, working_nodes, [], 30);
-let max_result = 0;
-for (const order of p1_orders) {
-  //console.log(order);
-  //max_result++;
-  const result = run_order(foo, start_node, order, 30);
-  if (result > max_result) max_result = result;
+console.log("#1", maxPressure);
+
+// part2
+
+time = 26;
+// build a database of [pressure, path] for all possible paths
+const database = [];
+for (const path of generatePaths(start, visit, [], time)) {
+  const result = pressureForPath(path, time);
+  database.push([result, path]);
 }
 
-console.log("#1", max_result);
+// sort high to low
+database.sort((a, b) => b[0] - a[0]);
+maxPressure = 0;
+// search for two paths with no intersecting valves
+for (const [pressure, myPath] of database) {
+  if (pressure < maxPressure / 2) break;
+  for (const [elephantPressure, elephantPath] of database) {
+    // if no intersection, valid combo
+    if (!elephantPath.some((room) => myPath.includes(room))) {
+      const totalPressure = pressure + elephantPressure;
+      if (totalPressure > maxPressure) maxPressure = totalPressure;
+    }
+  }
+}
+
+console.log("#2", maxPressure);
