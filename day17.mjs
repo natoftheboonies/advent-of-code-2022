a6dd9dacd202aecc3671abb2d83717d4376efe0b
Nat Peterson <natpeterson@gmail.com>
2022-12-17 17:45:14 -0800

    day17 tidy

--- day17.mjs
+++ day17.mjs
@@ -61,150 +61,111 @@ function drawChamber() {
   }
   console.log("+-------+");
 }
-//console.log(pieces);
 
-// witdh = 7
-// rocks appear +4 above base
+function playTetris(gameover = 2022) {
+  const toKey = ([x, y]) => y * 10 + x;
 
-// sample is 10 rocks
+  // store settled rocks and top
+  const chamber = new Set();
+  let top = -1;
 
-// store settled rocks and top
-const chamber = new Set();
-let top = -1;
+  let i = 0;
+  let jet = 0;
+  let heights = [];
+  let seen = new Map();
 
-const toKey = ([x, y]) => y * 10 + x;
-let i = 0;
-let jet = 0;
-let heights = [];
-let seen = new Map();
+  let tracking = false;
+  let trackingLevel = 0;
+  let tracks = [];
 
-let tracking = false;
-let trackingLevel = 0;
-let tracks = [];
-const sequence = [];
+  while (i < gameover) {
+    const rock = pieces.at(i % 5);
+    let pos = [2, top + 4];
+    const priorTop = top;
 
-while (i < 20220) {
-  const rock = pieces.at(i % 5);
-  let pos = [2, top + 4];
-  const priorTop = top;
-  //console.log("rock", i, i % 5, rock, "at", pos);
-  const dy = -1;
-
-  while (true) {
-    const dx = jets.at(jet % jets.length);
-    // can jet move rock?
-    if (
-      dx &&
-      rock.every(([x, y]) => {
-        let nx = x + pos[0] + dx;
-        let ny = y + pos[1];
-        let oob = nx > 6 || nx < 0;
-        let obs = chamber.has(toKey([nx, ny]));
-        return !oob && !obs;
-      })
-    ) {
-      pos[0] += dx;
-      //console.log("shift", dx == 1 ? "right" : "left");
-    } else {
-      //console.log("shift", dx == 1 ? "right" : "left", "blocked");
-    }
-    jet++;
-
-    // can rock drop?
-    if (
-      rock.every(([x, y]) => {
-        const nx = x + pos[0],
-          ny = y + pos[1] + dy;
-        const up = ny > -1;
-        const blocked = chamber.has(toKey([nx, ny]));
-        return up && !blocked;
-      })
-    ) {
-      pos[1] += dy;
-      //console.log("drop", dy);
-    } else {
-      //console.log("settle rock");
-      rock.forEach(([x, y]) => {
-        chamber.add(toKey([x + pos[0], y + pos[1]]));
-        top = Math.max(top, y + pos[1]);
-      });
-
-      break;
-    }
-
-    //console.log("now at", pos);
-  }
-  i++;
-  const height = top - priorTop;
-  heights.push(height);
-
-  if (heights.length > 30) {
-    const situation = `${jet % jets.length},${i % 5},${heights
-      .slice(-30)
-      .join(",")}`;
-    if (seen.has(situation)) {
-      //console.log("we've been here before", situation);
-      if (!tracking) {
-        console.log("start", top, i);
-        tracks.push([top, i]);
-        trackingLevel = seen.get(situation);
-        tracking = true;
-      } else if (trackingLevel < seen.get(situation)) {
-        console.log("reset", top, i);
-        tracks.push([top, i]);
-        trackingLevel = seen.get(situation);
+    while (true) {
+      const dx = jets.at(jet % jets.length);
+      // can jet move rock?
+      if (
+        dx &&
+        rock.every(([x, y]) => {
+          let nx = x + pos[0] + dx;
+          let ny = y + pos[1];
+          let oob = nx > 6 || nx < 0; // out of bounds
+          let obs = chamber.has(toKey([nx, ny])); // obstructed
+          return !oob && !obs;
+        })
+      ) {
+        pos[0] += dx;
       }
-      seen.set(situation, seen.get(situation) + 1);
-    } else {
-      if (tracking) {
-        console.log("stop", top);
-        tracking = false;
-      }
+      jet++;
 
-      seen.set(situation, 1);
+      const dy = -1;
+      // can rock drop?
+      if (
+        rock.every(([x, y]) => {
+          const nx = x + pos[0],
+            ny = y + pos[1] + dy;
+          const oob = ny < 0;
+          const obs = chamber.has(toKey([nx, ny]));
+          return !oob && !obs;
+        })
+      ) {
+        pos[1] += dy;
+      } else {
+        rock.forEach(([x, y]) => {
+          chamber.add(toKey([x + pos[0], y + pos[1]]));
+          top = Math.max(top, y + pos[1]);
+        });
+        break;
+      }
+    }
+    i++;
+    heights.push(top - priorTop);
+
+    if (heights.length > 30) {
+      const situation = `${jet % jets.length},${i % 5},${heights
+        .slice(-30)
+        .join(",")}`;
+      if (seen.has(situation)) {
+        if (!tracking) {
+          tracks.push([top, i]);
+          trackingLevel = seen.get(situation);
+          tracking = true;
+        } else if (trackingLevel < seen.get(situation)) {
+          tracks.push([top, i]);
+          trackingLevel = seen.get(situation);
+        }
+        seen.set(situation, seen.get(situation) + 1);
+      } else {
+        seen.set(situation, 1);
+      }
+    }
+    // prune set
+    for (const key of chamber) {
+      if (key / 10 + 50 < top) chamber.delete(key);
+    }
+    if (tracks.length > 1) {
+      // we have cycled!
+      const [startHeight, startBlocks] = tracks[0];
+
+      const cycleHeight = tracks[1][0] - startHeight;
+      const cycleBlocks = tracks[1][1] - startBlocks;
+
+      const mult = Math.floor((gameover - startBlocks) / cycleBlocks);
+
+      const tailBlocks = gameover - mult * cycleBlocks - startBlocks;
+      const tailHeight = heights
+        .slice(startBlocks, startBlocks + tailBlocks)
+        .reduce((a, b) => a + b);
+
+      const calcTop = startHeight + mult * cycleHeight + tailHeight;
+      return calcTop + 1;
     }
   }
-  // prune set
-  for (const key of chamber) {
-    if (key / 10 + 50 < top) chamber.delete(key);
-  }
-  //console.log(chamber.size);
+  return top + 1;
 }
-//drawChamber();
-console.log("top:", top + 1); // 2485 too low, 3081, 3082 too high
-//console.log(Math.max(...chamber));
 
-let x = 0;
-for (const situation of seen.keys()) {
-  if (seen.get(situation) > 10) {
-    //console.log(situation, seen.get(situation));
-    x++;
-  }
-}
-//console.log("cycle length", tracks, x);
-// cycle is 2582 height in 1705 blocks
+console.log("#1:", playTetris());
 
-console.log("h:", heights.length);
-
-const cycleHeight = tracks[3][0] - tracks[2][0];
-const cycleBlocks = tracks[3][1] - tracks[2][1];
-
-const [startHeight, startBlocks] = tracks[0];
-
-console.log(
-  "chk",
-  heights.slice(startBlocks, startBlocks + cycleBlocks).reduce((a, b) => a + b)
-);
-
-console.log("start b/h", startBlocks, startHeight);
-console.log("cycle (b, h)", cycleBlocks, cycleHeight);
-const mult = Math.floor((1e12 - startBlocks) / cycleBlocks);
-console.log("num cycles", mult);
-
-const tailBlocks = 1e12 - mult * cycleBlocks - startBlocks;
-console.log("tail b/h", tailBlocks);
-const tailHeight = heights
-  .slice(startBlocks, startBlocks + tailBlocks)
-  .reduce((a, b) => a + b);
-const part2 = startHeight + mult * cycleHeight + tailHeight + 1;
-console.log("#2:", part2);
+console.log("#2:", playTetris(1e12));
